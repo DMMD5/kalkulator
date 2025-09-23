9c2992b4a1a33ee7a85f082a07c00f8234ddb314
DMMD5 <b3romdor@vasvari.org>
2025-09-23 12:24:39 +0200

    Add project files.

---
+++ kalkulator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalkulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double elso;
+            double masodik;
+            string muvelet;
+
+            Console.Write("Add meg az első számot: ");
+            elso = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Add meg a második számot: ");
+            masodik = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Adjon meg egy műveleti jelet (+,-,*,/): ");
+            muvelet = Convert.ToString(Console.ReadLine());
+
+            switch (muvelet)
+            {
+                case "+":
+                    Console.WriteLine($"A megoldás: {elso + masodik}");
+                    break;
+
+                case "-":
+                    Console.WriteLine($"A megoldás: {elso - masodik}");
+                    break;
+
+                case "*":
+                    Console.WriteLine($"A megoldás: {elso * masodik}");
+                    break;
+
+                case "/":
+                    Console.WriteLine($"A megoldás: {elso / masodik}");
+                    break;
+                default:
+                    Console.WriteLine("Hibás művelet");
+                    break;
+
+            }
+
+            
+
+
+
+            
+        }
+    }
+}
